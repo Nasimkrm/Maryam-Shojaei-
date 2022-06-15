6d3cb49be3abae5cd1d15b3b576df0cc1970a51c
Nasimkrm <nassim.karimi.nk@gmail.com>
2022-06-15 17:20:51 +0200

    Changed font styles, font sizes

--- src/Footer.js
+++ src/Footer.js
@@ -11,11 +11,21 @@ const Footer = () => {
         <a href="https://www.instagram.com/noor.skincare.mariya/"><img className="icons-footer" src="./images/instagram.png" alt="" /></a>
         <a href="www.google.com"><img className="icons-footer" src="./images/telegramme.png" alt="" /></a>
         
+        
         </div>
 
         <p>تمامی حقوق محفوظ است</p>
         </div>
 
+        <div className="main-links">
+            <p className="link-title">تماس با ما</p>
+            <div className="number">
+            <img className="icons-footer" src="./images/whatsapp.png" alt="" /><a className="main-link" href="tel:+989139000955">+989051112231</a>
+            </div>
+            <div className="address">
+                <img className="icons-footer" src="./images/location.png" alt="" /><p className="address-text">بندر انزلی - ناصر خسرو </p>
+            </div>
+        </div>
 
         <div className="main-links">
             <p className="link-title">صفحات اصلی</p>
@@ -23,6 +33,9 @@ const Footer = () => {
             <a className="main-link" href="/yoga">یوگا و مدیتیشن</a>
             <a className="main-link" href="/facial">خدمات پوستی</a>
         </div>
+
+
+        
 
         </div>
      );
--- src/Navbar.js
+++ src/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
         
         <div className="icons-div">
         <a href="https://www.instagram.com/noor.skincare.mariya/"><img className="icons" src="./images/instagram.png" alt="" /></a>
-        <a href="www.google.com"><img className="icons" src="./images/telegramme.png" alt="" /></a>
+        <a href="https://t.me/noor_shojaee"><img className="icons" src="./images/telegramme.png" alt="" /></a>
         </div>
 
         
